299b39520937946f6a5a9f500fb7d0c664ee172a
anmunoz <andres.max36@gmail.com>
2019-10-08 16:44:43 +0200

    Add script files

---
+++ prediction-job/run-spark-jobs.sh
@@ -0,0 +1,5 @@
+#! /bin/bash -eu
+
+./spark/bin/spark-submit  --class  org.fiware.cosmos.orion.spark.connector.prediction.TrainingJob --master  spark://spark-master:7077 --deploy-mode client ./prediction-job/target/orion.spark.connector.prediction-1.0.1.jar
+./spark/bin/spark-submit  --class  org.fiware.cosmos.orion.spark.connector.prediction.PredictionJob --master  spark://spark-master:7077 --deploy-mode client ./prediction-job/target/orion.spark.connector.prediction-1.0.1.jar
+
